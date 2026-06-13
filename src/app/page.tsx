7b15ccf49b4480a5ad26dbfd0f86beffa708f022
jaseem <muhammedjaseemkc277@gmail.com>
2026-06-13 15:07:40 +0530

    updated howitworks and hided category from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,20 @@ import { StoreFeatures } from "@/components/sections/StoreFeatures"
 import { Testimonials } from "@/components/sections/Testimonials"
 import { SmileCTA } from "@/components/sections/SmileCTA"
 import ShopByOccasion from "@/components/sections/ShopByOcassion"
+import { CraftedGalleryHer } from "@/components/sections/gifts-for-her/CraftedGalleryHer"
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
       <ShopByOccasion/>
-      {/* <Discover /> */}
-      <Categories />
+      {/* <Discover /> */} 
+      {/* <Categories /> */}
       <HowItWorks />
       <FeaturedBanner />
       <ProductGrid />
       <StoreFeatures />
+      <CraftedGalleryHer />
       <Testimonials />
       <SmileCTA />
     </div>
